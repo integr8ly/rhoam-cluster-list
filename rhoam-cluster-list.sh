25e1be5c178dee36047a21a8ab7a19b4fe17eae0
Austin Cunningham <aucunnin@redhat.com>
2025-07-09 15:50:05 +0100

    Create rhoam-cluster-list.sh

---
+++ rhoam-cluster-list.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Get a list of all cluster IDs
+cluster_ids=$(ocm list clusters --columns id --no-headers)
+
+echo ''
+echo '       _                                        _           _                     _ _     _   '
+echo '      | |                                      | |         | |                   | (_)   | |  '
+echo '  _ __| |__   ___   __ _ _ __ ___  ______   ___| |_   _ ___| |_ ___ _ __  ______ | |_ ___| |_ '
+echo ' |  __|  _ \ / _ \ |__  |  _   _ \ |____|  / __| | | | / __| __/ _ \  __| |____| | | / __| __|'
+echo ' | |  | | | | (_) | (_| | | | | | |       | (__| | |_| \__ \ ||  __/ |           | | \__ \ |_ '
+echo ' |_|  |_| |_|\___/ \__,_|_| |_| |_|        \___|_|\__,_|___/\__\___|_|           |_|_|___/\__|'
+echo ''
+
+# Print the header for our table
+printf "%-35s %-25s %-15s %-20s\n" "CLUSTER ID" "NAME" "OPENSHIFT" "ADDON VERSION"
+printf "%-35s %-25s %-15s %-20s\n" "-----------------------------------" "-------------------------" "---------------" "--------------------"
+
+# Loop through each cluster ID
+for id in $cluster_ids; do
+  # Check if the RHOAM addon is installed and ready
+  if ocm list addons --cluster "$id" | grep 'managed-api-service-internal' | grep -q 'ready'; then
+    
+    # Get cluster and addon details
+    cluster_details=$(ocm describe cluster "$id")
+    # This now specifically gets the 'Name:' line, ignoring 'Display Name:'
+    cluster_name=$(echo "$cluster_details" | grep '^Name:' | awk '{$1=""; print $0}' | xargs)
+    ocp_version=$(echo "$cluster_details" | grep 'Version:' | awk '{$1=""; print $0}' | xargs)
+    addon_version=$(ocm get "/api/clusters_mgmt/v1/clusters/$id/addons" | jq -r '.items[] | select(.id == "managed-api-service-internal") | .addon_version.id')
+
+    # Print all details in a single, formatted horizontal line
+    printf "%-35s %-25s %-15s %-20s\n" "$id" "$cluster_name" "$ocp_version" "$addon_version"
+  fi
+done
+
+echo -e "\nSearch complete."
+
